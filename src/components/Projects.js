20fd60947277b1fa88aa022386086c170ef564d2
German Garcia Gonzalez <ggarciag.business@outlook.com>
2022-10-04 14:58:33 -0400

    projects animation is triggered when in view

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,7 @@ function Projects(){
     return(
         <div >
             <div className='projects-heading is-size-3'><motion.div whileHover={{ scale: 1.3 }}>P</motion.div><motion.div whileHover={{ scale: 1.3 }}>r</motion.div><motion.div whileHover={{ scale: 1.3 }}>o</motion.div><motion.div whileHover={{ scale: 1.3 }}>j</motion.div><motion.div whileHover={{ scale: 1.3 }}>e</motion.div><motion.div whileHover={{ scale: 1.3 }}>c</motion.div><motion.div whileHover={{ scale: 1.3 }}>t</motion.div><motion.div whileHover={{ scale: 1.3 }}>s</motion.div></div>
-            <motion.div animate={{x: 0}} initial={{x: -800}} transition={{type: 'tween', duration: 3}}
+            <motion.div whileInView={{x: 0}} initial={{x: -800}} transition={{type: 'tween', duration: 3}}
             className='projects'>
                 <div>
                     <img src={WQpic} className='wq-pic'/>
@@ -23,7 +23,7 @@ function Projects(){
                 </div>
                 </div>
             </motion.div>
-            <motion.div animate={{x: 0}} initial={{x: 800}} transition={{type: 'tween', duration: 3}}
+            <motion.div whileInView={{x: 0}} initial={{x: 800}} transition={{type: 'tween', duration: 3}}
             className='projects'>
                 <div className='il-details'>
                 <h3>Info-Land</h3>
