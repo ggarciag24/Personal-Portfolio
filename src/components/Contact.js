46f5dcfabfb0fb91f1f22807d0a7109e7e23f98a
German Garcia Gonzalez <ggarciag.business@outlook.com>
2022-10-06 11:55:05 -0400

    projects section has been styled

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,8 +9,8 @@ function Contact() {
         <motion.div whileHover={{ scale: 1.3, color: 'orange'}}>L</motion.div><motion.div whileHover={{ scale: 1.3, color: 'orange'}}>e</motion.div><motion.div whileHover={{ scale: 1.3, color: 'orange'}}>t</motion.div><motion.div whileHover={{ scale: 1.3, color: 'orange'}}>'</motion.div><motion.div whileHover={{ scale: 1.3, color: 'orange'}}>s</motion.div><motion.div whileHover={{ scale: 1.3, color: 'orange'}} className='gap'>T</motion.div><motion.div whileHover={{ scale: 1.3, color: 'orange'}}>a</motion.div><motion.div whileHover={{ scale: 1.3, color: 'orange'}}>l</motion.div><motion.div whileHover={{ scale: 1.3, color: 'orange'}}>k</motion.div>
         </div>
         <div className='contact-info'>
-        <motion.button className='button is-info is-rounded' whileHover={{ scale: 1.5}}><a href='https://www.linkedin.com/in/german-garcia-gonzalez/'>LinkedIn</a></motion.button>
-        <motion.button className='btn button is-info is-rounded' whileHover={{ scale: 1.5}}><a href='https://github.com/ggarciag24' target={'_blank'}>Github</a></motion.button>
+        <motion.button className='button is-info is-rounded' whileHover={{ scale: 1.5}}><a href='https://www.linkedin.com/in/german-garcia-gonzalez/' target='_blank'>LinkedIn</a></motion.button>
+        <motion.button className='btn button is-info is-rounded' whileHover={{ scale: 1.5}}><a href='https://github.com/ggarciag24' target='_blank'>Github</a></motion.button>
         <motion.button className='btn button is-info is-rounded' whileHover={{ scale: 1.5}}><a href="mailto:ggarciag.business@outlook.com">Email</a></motion.button>
         </div>
         </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,24 +14,24 @@ function Projects(){
                     <img src={WQpic} className='wq-pic'/>
                 </div>
                 <div className='wq-details'>
-                <h3>Word-Quiz</h3>
-                <p> React, HTML, CSS, Bulma</p>
-                <p>About</p>
-                <div>
-                    <button>Website</button>
-                    <button>Code</button>
+                <h1 className='is-size-3'>Word-Quiz</h1>
+                <p> React, HTML, CSS, Jest, Bulma</p>
+                <p className='project-about'>Random defintions are generated for the user to guess the correct word that belongs to it. The user can also click for hints if needed.</p>
+                <div className='project-btns'>
+                    <button className='button is-warning'>Website</button>
+                    <button className='code-btn button is-warning'><a href='https://github.com/ggarciag24/word-quiz' target='_blank'>Code</a></button>
                 </div>
                 </div>
             </motion.div>
             <motion.div whileInView={{x: 0}} initial={{x: 800}} transition={{type: 'tween', duration: 3}}
             className='projects'>
                 <div className='il-details'>
-                <h3>Info-Land</h3>
-                <p> React, HTML, CSS</p>
-                <p>About</p>
-                <div>
-                    <button>Website</button>
-                    <button>Code</button>
+                <h1 className='is-size-3'>Info-Land</h1>
+                <p> React, HTML, CSS, Jest</p>
+                <p className='project-about'>A user can search for a specific country and get more information on that country. The user can also look through a list of all the countries and sort and/or filter the list. Additionally the user can click the map button and be directed to the location of that country on google maps.</p>
+                <div className='project-btns'>
+                    <button className='button is-warning'>Website</button>
+                    <button className='code-btn button is-warning'><a href='https://github.com/ggarciag24/info-land' target='_blank'>Code</a></button>
                 </div>
                 </div>
                 <div>
